a800e07a12393783ebfad5719e087679c5752e4e
Code Sandbox <hello@codesandbox.io>
2023-02-16 11:03:29 +0000

    Deployment from Codesandbox.

---
+++ precache-manifest.9660c7638d6f14d7c31f4d75a225b111.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cb4cf561a07ad2e72bf2eb778d6e87b8",
+    "url": "https://madebybrown.github.io/csb-go1y78/index.html"
+  },
+  {
+    "revision": "e61dcfd1e7579c80d077",
+    "url": "https://madebybrown.github.io/csb-go1y78/static/css/main.895c32ce.chunk.css"
+  },
+  {
+    "revision": "2fc3a97a2795a0da7278",
+    "url": "https://madebybrown.github.io/csb-go1y78/static/js/2.4e288ace.chunk.js"
+  },
+  {
+    "revision": "e61dcfd1e7579c80d077",
+    "url": "https://madebybrown.github.io/csb-go1y78/static/js/main.e9495271.chunk.js"
+  },
+  {
+    "revision": "ad96e08e1429064c8f45",
+    "url": "https://madebybrown.github.io/csb-go1y78/static/js/runtime~main.0c058594.js"
+  },
+  {
+    "revision": "02c60ba13c8c5bba19e8eba20526d518",
+    "url": "https://madebybrown.github.io/csb-go1y78/static/media/Cables.02c60ba1.PNG"
+  },
+  {
+    "revision": "7d343757d93ca745d2a8b0fd87af41ba",
+    "url": "https://madebybrown.github.io/csb-go1y78/static/media/Laser.7d343757.PNG"
+  },
+  {
+    "revision": "c15151b85eaa90eb47516cf2d6868c3f",
+    "url": "https://madebybrown.github.io/csb-go1y78/static/media/Logo.c15151b8.png"
+  },
+  {
+    "revision": "14840a09bbe57cb8e44b2593c8498669",
+    "url": "https://madebybrown.github.io/csb-go1y78/static/media/Machinery.14840a09.PNG"
+  },
+  {
+    "revision": "43c3fcbe1cae34bb8fc2e01ebf3586f4",
+    "url": "https://madebybrown.github.io/csb-go1y78/static/media/MotorColor.43c3fcbe.PNG"
+  },
+  {
+    "revision": "d0caa1109a58c12bd477a575684fc718",
+    "url": "https://madebybrown.github.io/csb-go1y78/static/media/Printing.d0caa110.PNG"
+  },
+  {
+    "revision": "38d8f91cb8eefb9700082607f45bb1b9",
+    "url": "https://madebybrown.github.io/csb-go1y78/static/media/Rework.38d8f91c.PNG"
+  }
+]);
